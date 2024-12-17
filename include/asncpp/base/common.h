e286a642ee55ee12c30dbe06f88ff75e8d1b0efc
Kanduba Nikita <kandubanikita@gmail.com>
2024-12-17 20:14:59 +0200

    refactor: Класс asn1_basic вынесен в отдельный исходный файлrefactor: Класс asn1_basic добавлен в область имен asncpp

--- include/asncpp/base/common.h
+++ include/asncpp/base/common.h
@@ -8,9 +8,7 @@
 #include <algorithm>
 #include <vector>
 #include <span>
-#include <stdexcept>
 #include <variant>
-#include <iterator>
 #include <memory>
 
 
@@ -75,213 +73,19 @@ namespace asncpp::base {
     auto deserialize_v(std::span<const uint8_t> data) -> std::unique_ptr<asn1_basic>;
 
     auto serialize(asn1_basic *block) -> std::vector<uint8_t>;
-}  // namespace asncpp::base
 
+    std::pair<tag_t, size_t> extract_type(std::span<const uint8_t> buffer);
 
+    dynamic_array_t encode_length(size_t length);
 
+    dynamic_array_t encode_type();
 
-//
-// class asncpp::base::asn1_basic {
-// public:
-//     using dynamic_data_t = std::vector<uint8_t>;
-//
-//     asn1_basic() = default;
-//
-//     virtual ~asn1_basic() = default;
-//
-//     asn1_basic(std::span<const uint8_t> data) {
-//         decode(data);
-//     }
-//
-//     [[nodiscard]] bool is_constructed() const noexcept {
-//         return _constructed;
-//     }
-//
-//     [[nodiscard]] tag_t get_type() const noexcept {
-//         return _type;
-//     }
-//
-//     [[nodiscard]] asn1_class get_cls() const noexcept {
-//         return _cls;
-//     }
-//
-//     [[nodiscard]] size_t get_length() const noexcept {
-//         return _length;
-//     }
-//
-//     [[nodiscard]] const dynamic_data_t &get_data() const noexcept {
-//         return _data;
-//     }
-//
-//     [[nodiscard]] virtual std::string to_string() const {
-//         return "ASN.1 Base";
-//     }
-//
-//
-//     friend auto serialize(asn1_basic *block) -> std::vector<uint8_t>;
-//
-//     friend auto deserialize_v(std::span<const uint8_t> data) -> std::unique_ptr<asn1_basic>;
-//
-//     template<typename T>
-//     friend std::unique_ptr<T> deserialize(std::span<const uint8_t> data) {
-//         if (data.empty()) {
-//             throw std::invalid_argument("Invalid ASN.1 data");
-//         }
-//         const auto type = asn1_basic::extract_type(data).first;
-//         if (!std::get_if<asn1_tag>(&type)) {
-//             return nullptr;
-//         }
-//         std::unique_ptr ptr = std::make_unique<T>();
-//         (void) static_cast<asn1_basic *>(ptr.get())->asn1_basic::decode(data);
-//         ptr->decode(data);
-//         return ptr;
-//     }
-//
-// protected:
-//     virtual void decode(std::span<const uint8_t> data) {
-//         if (data.size_bytes() < 2) {
-//             throw std::invalid_argument("Invalid ASN.1 data. Length is too short");
-//         }
-//
-//         this->_cls = extract_class(data[0]);
-//         this->_constructed = extract_is_constructed(data[0]);
-//         const auto type_decoded = extract_type(data);
-//         data = data.subspan(type_decoded.second);
-//         const auto length = extract_length(data);
-//         data = data.subspan(length.second);
-//         this->_type = type_decoded.first;
-//         this->_length = length.first;
-//         const auto it = data.begin();
-//         if (it + length.first > data.end()) {
-//             throw std::invalid_argument("Invalid ASN.1 data. Length exceeds buffer size");
-//         }
-//         _data = std::vector<uint8_t>(it, it + static_cast<std::ptrdiff_t>(length.first));
-//     }
-//
-//     [[nodiscard]] virtual std::vector<uint8_t> encode() {
-//         dynamic_data_t output = _data;
-//         output.insert_range(output.begin(), asn1_basic::encode_length(_data.size()));
-//         output.insert_range(output.begin(), encode_type());
-//         return output;
-//     }
-//
-//     dynamic_data_t _data;
-//     tag_t _type;
-//
-// private:
-//     bool _constructed{};
-//
-//     asn1_class _cls{};
-//     size_t _length{};
-//
-//     //TODO: Проверка длинны массива
-//     [[nodiscard]] static auto extract_type(std::span<const uint8_t> buffer) -> std::pair<tag_t, size_t> {
-//         const uint8_t tag_decoded = buffer[0] & 0x1FU;
-//         if (tag_decoded < 0x1FU) {
-//             return std::make_pair(static_cast<asn1_tag>(tag_decoded), 1ULL);
-//         }
-//         auto it = std::next(buffer.begin());
-//         uintmax_t type_decoded{0};
-//         const auto push_bits = [&](const uint8_t byte) {
-//             if (it - buffer.begin() - 1 > sizeof(uintmax_t)) {
-//                 throw std::runtime_error("Tag is too long");
-//             }
-//             type_decoded = (type_decoded << 7U) | (byte & 0x7FU);
-//         };
-//         while (*it & 0x80U) {
-//             push_bits(*it);
-//             ++it;
-//         }
-//         push_bits(*it);
-//         ++it;
-//         if (type_decoded <= 0x22U) {
-//             return std::make_pair(static_cast<asn1_tag>(type_decoded), it - buffer.begin());
-//         }
-//         return std::make_pair(type_decoded, it - buffer.begin());
-//     }
-//
-//     [[nodiscard]] static auto encode_length(size_t length) -> std::vector<uint8_t> {
-//         if (length <= 127) {
-//             return {static_cast<uint8_t>(length)};
-//         }
-//         std::vector<uint8_t> output;
-//         output.reserve(sizeof(size_t));
-//         while (length > 0) {
-//             output.emplace(output.begin(), static_cast<uint8_t>(length & 0xFFU));
-//             length >>= 8U;
-//         }
-//         output.emplace(output.begin(), static_cast<uint8_t>(output.size() | 0x80U));
-//         return output;
-//     }
-//
-//     [[nodiscard]] auto encode_type() -> std::vector<uint8_t> {
-//         std::vector<uint8_t> output;
-//         uintmax_t raw_type{};
-//
-//         if (std::get_if<asn1_tag>(&_type)) {
-//             raw_type = static_cast<uintmax_t>(std::get<asn1_tag>(_type));
-//         } else {
-//             raw_type = std::get<uintmax_t>(_type);
-//         }
-//         if (raw_type < 0x1FU) {
-//             return {
-//                 static_cast<unsigned char>(static_cast<uint8_t>(get_cls()) << 6U |
-//                                            static_cast<uint8_t>(is_constructed()) << 5U |
-//                                            static_cast<uint8_t>(raw_type))
-//             };
-//             //output.push_back(static_cast<uint8_t>(raw_type))};
-//             //return output;
-//         }
-//
-//
-//         std::vector<uint8_t> result;
-//         result.push_back(static_cast<unsigned char>(static_cast<uint8_t>(get_cls()) << 6U |
-//                                                     static_cast<uint8_t>(is_constructed()) << 5U |
-//                                                     static_cast<uint8_t>(0x1FU)));
-//         do {
-//             result.push_back(static_cast<uint8_t>(raw_type & 0x7FU));
-//             raw_type >>= 7U;
-//         } while (raw_type > 0);
-//
-//         std::transform(std::next(result.begin()),
-//                        std::prev(result.end()),
-//                        std::next(result.begin()),
-//                        [](const uint8_t byte) {
-//                            return byte | 0x80U;
-//                        });
-//         return result;
-//     }
-//
-//     //TODO: Implement long tag encoding
-//     //TODO: Проверка длинны массива
-//     [[nodiscard]] static auto extract_length(std::span<const uint8_t> buffer) -> std::pair<size_t, size_t> {
-//         if (!(buffer[0] & 0x80U)) {
-//             return std::make_pair(buffer[0], 1ULL);
-//         }
-//
-//         size_t length = 0;
-//         const uint8_t num_octets = buffer[0] & 0x7FU;
-//         if (num_octets > sizeof(size_t)) {
-//             throw std::runtime_error("Length is too long");
-//         }
-//         for (size_t i = 1; i <= num_octets; i++) {
-//             length = (length << 8U) | buffer[i];
-//         }
-//         return std::make_pair(length, num_octets + 1);
-//     }
-//
-//     [[nodiscard]] static auto extract_is_constructed(const uint8_t tag) noexcept -> bool {
-//         return (tag & 0x20U) >> 5U;
-//     }
-//
-//     [[nodiscard]] static auto extract_class(const uint8_t tag) noexcept -> asn1_class {
-//         return static_cast<asn1_class>((tag & 0xC0U) >> 6U);
-//     }
-// };
-//
+    std::pair<size_t, size_t> base::extract_length(std::span<const uint8_t> buffer);
 
-// template<typename T>
-// auto deserialize(std::span<const uint8_t> data) -> std::unique_ptr<T>;
+    bool extract_is_constructed(const uint8_t tag) noexcept;
+
+    asn1_class extract_class(const uint8_t tag) noexcept;
+} // namespace asncpp::base
 
 
 #endif //ASNCPP_ASN_BASE_H
--- src/asn1_basic.cpp
+++ src/asn1_basic.cpp
@@ -24,9 +24,111 @@ void asncpp::base::asn1_basic::decode(std::span<const uint8_t> data) {
     _data = std::vector<uint8_t>(it, it + static_cast<std::ptrdiff_t>(length.first));
 }
 
-auto asncpp::base::asn1_basic::encode() -> asncpp::base::dynamic_array_t {
+asncpp::base::dynamic_array_t asncpp::base::asn1_basic::encode() {
     dynamic_array_t output = _data;
     output.insert_range(output.begin(), asn1_basic::encode_length(_data.size()));
     output.insert_range(output.begin(), encode_type());
     return output;
+}
+
+//TODO: Проверка длинны массива
+std::pair<asncpp::base::tag_t, size_t> asncpp::base::extract_type(std::span<const uint8_t> buffer) {
+    const uint8_t tag_decoded = buffer[0] & 0x1FU;
+    if (tag_decoded < 0x1FU) {
+        return std::make_pair(static_cast<asn1_tag>(tag_decoded), 1ULL);
+    }
+    auto it = std::next(buffer.begin());
+    uintmax_t type_decoded{0};
+    const auto push_bits = [&](const uint8_t byte) {
+        if (it - buffer.begin() - 1 > sizeof(uintmax_t)) {
+            throw std::runtime_error("Tag is too long");
+        }
+        type_decoded = (type_decoded << 7U) | (byte & 0x7FU);
+    };
+    while (*it & 0x80U) {
+        push_bits(*it);
+        ++it;
+    }
+    push_bits(*it);
+    ++it;
+    if (type_decoded <= 0x22U) {
+        return std::make_pair(static_cast<asn1_tag>(type_decoded), it - buffer.begin());
+    }
+    return std::make_pair(type_decoded, it - buffer.begin());
+}
+
+asncpp::base::dynamic_array_t asncpp::base::encode_length(size_t length) {
+    if (length <= 127) {
+        return {static_cast<uint8_t>(length)};
+    }
+    std::vector<uint8_t> output;
+    output.reserve(sizeof(size_t));
+    while (length > 0) {
+        output.emplace(output.begin(), static_cast<uint8_t>(length & 0xFFU));
+        length >>= 8U;
+    }
+    output.emplace(output.begin(), static_cast<uint8_t>(output.size() | 0x80U));
+    return output;
+}
+
+asncpp::base::dynamic_array_t asncpp::base::encode_type() {
+    std::vector<uint8_t> output;
+    uintmax_t raw_type{};
+
+    if (std::get_if<asn1_tag>(&_type)) {
+        raw_type = static_cast<uintmax_t>(std::get<asn1_tag>(_type));
+    } else {
+        raw_type = std::get<uintmax_t>(_type);
+    }
+    if (raw_type < 0x1FU) {
+        return {
+            static_cast<unsigned char>(static_cast<uint8_t>(get_cls()) << 6U |
+                                       static_cast<uint8_t>(is_constructed()) << 5U |
+                                       static_cast<uint8_t>(raw_type))
+        };
+    }
+
+
+    std::vector<uint8_t> result;
+    result.push_back(static_cast<unsigned char>(static_cast<uint8_t>(get_cls()) << 6U |
+                                                static_cast<uint8_t>(is_constructed()) << 5U |
+                                                static_cast<uint8_t>(0x1FU)));
+    do {
+        result.push_back(static_cast<uint8_t>(raw_type & 0x7FU));
+        raw_type >>= 7U;
+    } while (raw_type > 0);
+
+    std::transform(std::next(result.begin()),
+                   std::prev(result.end()),
+                   std::next(result.begin()),
+                   [](const uint8_t byte) {
+                       return byte | 0x80U;
+                   });
+    return result;
+}
+
+//TODO: Implement long tag encoding
+//TODO: Проверка длинны массива
+std::pair<size_t, size_t> asncpp::base::extract_length(std::span<const uint8_t> buffer) {
+    if (!(buffer[0] & 0x80U)) {
+        return std::make_pair(buffer[0], 1ULL);
+    }
+
+    size_t length = 0;
+    const uint8_t num_octets = buffer[0] & 0x7FU;
+    if (num_octets > sizeof(size_t)) {
+        throw std::runtime_error("Length is too long");
+    }
+    for (size_t i = 1; i <= num_octets; i++) {
+        length = (length << 8U) | buffer[i];
+    }
+    return std::make_pair(length, num_octets + 1);
+}
+
+bool asncpp::base::extract_is_constructed(const uint8_t tag) noexcept {
+    return (tag & 0x20U) >> 5U;
+}
+
+asncpp::base::asn1_class asncpp::base::extract_class(const uint8_t tag) noexcept {
+    return static_cast<asncpp::base::asn1_class>((tag & 0xC0U) >> 6U);
 }
